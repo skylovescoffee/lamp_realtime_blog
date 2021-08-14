6c204708e71f22db6e7a2fcd874ab1b4e70d5899
skylovescoffee <skylarjbruce@gmail.com>
2021-08-13 21:03:32 -0400

    init

---
+++ scripts/getUrl.php
@@ -0,0 +1,11 @@
+<?php
+function getUrl($path)
+{
+  if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+    $uri = 'https://';
+  } else {
+    $uri = 'http://';
+  }
+  $uri .= $_SERVER['HTTP_HOST'];
+  echo $uri . $path;
+}
